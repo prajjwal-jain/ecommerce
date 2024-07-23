7cc9d9e4863d48e45afa9437895fbf31724ffd8a
Prajjwal <prajjwal.jain1008@gmail.com>
2024-07-23 12:50:20 +0530

    code updated

--- client/src/pages/Policy.js
+++ client/src/pages/Policy.js
@@ -17,12 +17,7 @@ const Policy = () => {
         </div>
         <div className="col-md-4">
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+            made by prajjwal
           </p>
         </div>
       </div>
